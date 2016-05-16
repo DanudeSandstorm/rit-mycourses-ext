fe3f7abbe82716ea008f371fd05f84faf277aa13
Daniel <dds2130@rit.edu>
2016-05-16 14:24:34 -0400

    removes header of popout content

---
+++ scripts/popout-content.js
@@ -0,0 +1,28 @@
+
+function popoutContent(){
+
+	var header_height = removeHeader();
+	fixHeight(header_height);
+}
+
+function removeHeader(){
+	var header = document.getElementsByClassName("d2l-popup-title")[0];
+	var header_height = header.offsetHeight;
+
+	header.parentNode.removeChild(header);
+
+	return header_height;
+}
+
+function fixHeight(header_height) {
+	//Readjust height of div to compensate for removed header
+	var popup_body = document.getElementsByClassName('d2l-popup-body')[0];
+	var popup_body_height = popup_body.offsetHeight;
+	var view_height = popup_body_height + header_height;
+	popup_body.style.height = view_height + "px";
+
+	
+
+}
+
+popoutContent();
