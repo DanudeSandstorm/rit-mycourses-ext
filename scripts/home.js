d1918b999f0d198e1a62f518afb0c25a07957159
Daniel <dds2130@rit.edu>
2016-05-18 01:49:43 -0400

    adds widget header to the courses widget

--- scripts/home.js
+++ scripts/home.js
@@ -1,14 +1,30 @@
 'use strict';
 
+//Removes the top widgets from each column on the home page
 function removeContent() {
+	var widgets;
 
+	//Grabs the two columns on the home page
 	var boxes = document.querySelectorAll(".d2l-homepage .d2l-box .d2l-box-layout")[0].children;
-	boxes = [].slice.call(boxes);
-	boxes.forEach(function(box) {
-		console.log(box);
-		var widget = box.getElementsByClassName("d2l-widget")[0];
+	
+	//Grabs first column's first widget
+	widgets = getWidgets(boxes[0]);
+	//Grabs the widget header to place on the next box before removing
+	var header = widgets[0].getElementsByClassName("d2l-widget-header")[0];
+	widgets[1].insertBefore(header, widgets[1].firstChild);
+	removeWidget(widgets[0]);
+
+	//Grabs the second column's first widget
+	widgets = getWidgets(boxes[1]);
+	removeWidget(widgets[0]);
+
+	function getWidgets(node){
+		return node.getElementsByClassName("d2l-widget");
+	}
+
+	function removeWidget(widget){
 		widget.parentNode.removeChild(widget);
-	});
+	}
 }
 
 removeContent();
