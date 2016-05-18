087fe9fa98da56f970e8e9a6ec6f2dc59f845076
Daniel <dds2130@rit.edu>
2016-05-17 20:21:22 -0400

    removes 2px between navbar and header bar

---
+++ scripts/base.js
@@ -0,0 +1,10 @@
+'use strict';
+
+function navbarSpace(){
+	var navbar = document.getElementsByClassName("d2l-navbar-container")[0];
+	if (navbar != null) {
+		navbar.style.borderTop = "0";
+	}
+}
+
+navbarSpace();
