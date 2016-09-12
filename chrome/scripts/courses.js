59ac567dfd3a37b49bcf120796dab7bc5bd1a1cf
Daniel <dds2130@rit.edu>
2016-09-12 17:30:23 -0400

    reduces the length of course title in nav bar allows the content to fit on smaller screen widths

--- chrome/scripts/courses.js
+++ chrome/scripts/courses.js
@@ -1,6 +1,7 @@
 'use strict';
 
 function ritNavbar() {
+	//Reduces height of navbar
 	var rit_navbar = document.querySelectorAll(".d2l-navbar-container .d2l-box-layout > .d2l-box");
 	var mylibrary = rit_navbar[0].querySelectorAll(".d2l-box.d2l-box-h")[1];
 	
@@ -8,6 +9,10 @@ function ritNavbar() {
 	class_navbar.appendChild(mylibrary);
 
 	rit_navbar[0].parentNode.removeChild(rit_navbar[0]);
+
+	//Removes unnessisary portion of class title
+	var class_title = document.getElementsByClassName("d2l-navbar-title")[0];
+	class_title.innerHTML = class_title.innerHTML.replace(/ *\([^)]*\) */g, "").trim();
 }
 
 ritNavbar();
