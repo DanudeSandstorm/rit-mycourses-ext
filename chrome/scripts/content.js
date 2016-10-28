090958633f9d85ae062e872f39ed48e3131a8295
Daniel <dds2130@rit.edu>
2016-10-28 11:34:47 -0400

    fixed content popup glitch

--- chrome/scripts/content.js
+++ chrome/scripts/content.js
@@ -29,9 +29,9 @@ chrome.storage.sync.get("content_popout", function(data) {
 
 		window.addEventListener('DOMContentLoaded', function() {
 			replaceLinks();
-			var target = document.getElementsByClassName("d2l-body")[0];
+			var target = document.getElementsByClassName("d2l-twopanelselector-wrapper")[0];
 			//var target = document.querySelectorAll(".d2l-twopanelselector-wrapper .d2l-box")[1];
-			var config = { childList: true, substree: true };
+			var config = { childList: true, subtree: true };
 			console.log(target);
 			observer.observe(target, config);
 		}, false);
