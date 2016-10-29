c6dff93e4882f1b4a672bbc05750e17c7751ddda
Daniel <dds2130@rit.edu>
2016-10-29 19:19:21 -0400

    working on #12

---
+++ pack.sh
@@ -0,0 +1,30 @@
+if [ "$1" != "chrome" ] && [ "$1" != "firefox" ]; then
+  echo "Not a valid extension";
+  exit;
+fi
+
+echo "Packing shared files into "$1;
+
+[ -d $1/images ] || mkdir -p $1/images;
+for i in $( ls shared/images); do
+  cp shared/images/$i $1/images/;
+done
+
+[ -d $1/styles ] || mkdir -p $1/styles;
+for i in $( ls shared/styles); do
+  cp shared/styles/$i $1/styles/;
+done
+
+[ -d $1/lib ] || mkdir -p $1/lib;
+for i in $( ls shared/lib); do
+  cp shared/lib/$i $1/lib/;
+done
+
+# [ -d $1/scripts ] || mkdir -p $1/scripts;
+# for i in $( ls shared/scripts); do
+#   if [ -d $1/scripts/$1 ]; then
+#     cat 
+#   else
+#     cp shared/scripts/$i chrome/scripts/;
+#   fi 
+# done
