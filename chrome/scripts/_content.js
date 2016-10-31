03bdbd3cfeb26c2d00b509b4ff6b59105c00ba79
Daniel <dds2130@rit.edu>
2016-10-31 19:47:19 -0400

    Refactor
    Increased shared functionality
    Reduced non-shared code

--- chrome/scripts/_content.js
+++ chrome/scripts/_content.js
@@ -2,20 +2,8 @@ function openPopup(url) {
     chrome.runtime.sendMessage({ command: "createWindow", params: { url: url, focused: true, type: 'popup' } });
 }
 
-chrome.storage.sync.get("content_popout", function(data) {
-	if (data["content_popout"]) {
-		var observer = new MutationObserver(function(mutations) {
-			console.log('test');
-			replaceLinks();
-		});
-
-		window.addEventListener('DOMContentLoaded', function() {
-			replaceLinks();
-			var target = document.getElementsByClassName("d2l-twopanelselector-wrapper")[0];
-			//var target = document.querySelectorAll(".d2l-twopanelselector-wrapper .d2l-box")[1];
-			var config = { childList: true, subtree: true };
-			console.log(target);
-			observer.observe(target, config);
-		}, false);
-	}
-});
+function getContentData(callback) {
+	chrome.storage.sync.get("content_popout", function(data) {
+		return callback(data["content_popout"]);
+	});
+};
--- shared/scripts/content.js
+++ shared/scripts/content.js
@@ -1,5 +1,27 @@
 'use strict';
 
+//Checks for user settings
+getContentData(function(bool) {
+	if (bool) attachObserver();
+});
+
+//ReplaceLinks observer on content loaded
+function attachObserver() {
+	var observer = new MutationObserver(function(mutations) {
+		console.log('test');
+		replaceLinks();
+	});
+
+	window.addEventListener('DOMContentLoaded', function() {
+		replaceLinks();
+		var target = document.getElementsByClassName("d2l-twopanelselector-wrapper")[0];
+		//var target = document.querySelectorAll(".d2l-twopanelselector-wrapper .d2l-box")[1];
+		var config = { childList: true, subtree: true };
+		console.log(target);
+		observer.observe(target, config);
+	}, false);
+}
+
 //Replaces content links with popout content links
 function replaceLinks() {
 	//For each link replace with onclick event \
