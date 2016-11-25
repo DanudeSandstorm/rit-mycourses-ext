c8fce0d126a4b52cc44ce8420ee60ada0133cecd
Daniel <dds2130@rit.edu>
2016-11-25 14:50:24 -0500

    added functions to check for modified

--- pack.sh
+++ pack.sh
@@ -1,3 +1,26 @@
+#Checks if files have been modified
+#Params source/file, bin/file
+replacemodified()
+{
+  if [ ! -e "$2" ] || [ "$1" -nt "$2" ]; then
+    cp "$1" "$2";
+  fi
+  return 0
+}
+#Checks if files to merge, checks modified
+#Params shared/file, browser/_file, bin/file
+merge()
+{
+  if [ -f "$2" ]; then
+    if [ ! -e "$3" ] || [ "$1" -nt "$3" ] || [ "$2" -nt "$3" ]; then
+      cat "$1" "$2" > "$3";
+    fi
+  else
+    replacemodified "$1" "$3";
+  fi
+ return 0
+}
+
 if [ "$1" != "chrome" ] && [ "$1" != "firefox" ]; then
   echo "Not a valid extension";
   exit;
@@ -8,49 +31,50 @@ echo "copying shared files into $1";
 #pack images
 [ -d bin/$1/images ] || mkdir -p bin/$1/images;
 for i in $( ls shared/images); do
-  cp shared/images/$i bin/$1/images/;
+  replacemodified shared/images/$i bin/$1/images/;
 done
+#browser specific images
 for i in $( ls $1/images); do
-  cp $1/images/$i bin/$1/images/;
+  replacemodified $1/images/$i bin/$1/images/;
 done
 
 #pack libraries
 [ -d bin/$1/lib ] || mkdir -p bin/$1/lib;
 for i in $( ls shared/lib); do
-  cp shared/lib/$i bin/$1/lib/;
+  replacemodified shared/lib/$i bin/$1/lib/;
+done
+#browser specific images
+for i in $( ls shared/lib); do
+  replacemodified $1/lib/$i bin/$1/lib/;
 done
 
 #combine and pack styles (css)
 [ -d bin/$1/styles ] || mkdir -p bin/$1/styles;
 for i in $( ls shared/styles); do
-  if [ -f $1/styles/_$i ]; then
-    cat shared/styles/$i $1/styles/_$i > bin/$1/styles/$i;
-  else
-    cp shared/styles/$i bin/$1/styles/;
-  fi
+  merge shared/styles/$i $1/styles/_$i bin/$1/styles/$i;
 done
+#browser specific styles
 for i in $( ls $1/styles); do
   if [[ $i != _* ]]; then
-    cp $1/styles/$i bin/$1/styles/;
+    replacemodified $1/styles/$i bin/$1/styles/;
   fi
 done
 
 #combine and pack scripts
 [ -d bin/$1/scripts ] || mkdir -p bin/$1/scripts;
 for i in $( ls shared/scripts); do
-  if [ -f $1/scripts/_$i ]; then
-    cat shared/scripts/$i $1/scripts/_$i > bin/$1/scripts/$i;
-  else
-    cp shared/scripts/$i bin/$1/scripts/;
-  fi
+  merge shared/scripts/$i $1/scripts/_$i bin/$1/scripts/$i;
 done
+#browser specific scripts
 for i in $( ls $1/scripts); do
   if [[ $i != _* ]]; then
-    cp $1/scripts/$i bin/$1/scripts/;
+    replacemodified $1/scripts/$i bin/$1/scripts/;
   fi
 done
 
-#pack any other files
+#pack any other browser exclusive files
 for i in $( find $1 -maxdepth 1 -type f ); do
-  cp $i bin/$1/;
+  replacemodified $i bin/$1/;
 done
+
+exit 0
