44c7be4b65fece1b981f525ed93e906df7a97b10
Daniel <dds2130@rit.edu>
2018-11-12 18:47:08 -0500

    closes #29, fixes #28

--- chrome/scripts/background.js
+++ chrome/scripts/background.js
@@ -1,9 +1,9 @@
 'use strict';
 
 //http://stackoverflow.com/questions/26246601/wildcard-string-comparison-in-javascript
-function matchRule(str, rule) {
-    return new RegExp("^" + rule.split("*").join(".*") + "$").test(str);
-}
+// function matchRule(str, rule) {
+//     return new RegExp("^" + rule.split("*").join(".*") + "$").test(str);
+// }
 
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
